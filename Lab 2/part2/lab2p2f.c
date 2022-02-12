c96bf69c01184b479c8fb10add9657cc8d5f3362
Chen-Yiyang <iycspfa@gmail.com>
2022-02-12 17:13:54 +0800

    Finish part 2.7 code

--- Lab 2/part2/lab2p2f.c
+++ Lab 2/part2/lab2p2f.c
@@ -23,5 +23,45 @@ int main() {
      - remember to redirect final outputs
     */
 
+    int fp_out = open("./results.out", O_CREAT | O_WRONLY);
+
+    if(fork() == 0) {
+        dup2(fp_out, STDOUT_FILENO);
+
+        // pipe creation
+        // p[0] is the reading end, p[1] is the writing end.
+        int p[2];
+        
+        if(pipe(p) < 0) {
+            perror("lab2p2f: ");
+        }
+        
+        // We will send a message from father to child
+        if(fork() != 0) { // parent is TALK
+            close(p[1]);
+
+            wait(NULL); // let child write first
+            
+            dup2(p[0], STDIN_FILENO);
+            dup2(fp_out, STDOUT_FILENO);
+            
+            execlp("./talk", "talk", (char *) 0);
+            
+            
+            close(p[0]);
+            
+        }
+        else { // child is SLOW
+            close(p[0]);
+            dup2(p[1], STDOUT_FILENO);
+
+            execlp("./slow", "slow", "5", (char *) 0);
+
+            close(p[1]);
+        }
+    }
+    else
+        wait(NULL);
+
 }
 
